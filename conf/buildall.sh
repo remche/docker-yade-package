4af9d4ddd09f0a4f7e147924a78fb67ce6ec7493
remche <remche@remche.org>
2017-01-18 17:39:01 +0100

    added packages installation check

--- conf/buildall.sh
+++ conf/buildall.sh
@@ -5,11 +5,9 @@ distros=(jessie stretch unstable trusty xenial yakkety)
 cd docker-yade-package
 for distro in ${distros[@]}; do
     echo "Building for $distro"
-    DISTR=$distro /usr/local/bin/docker-compose -p $distro up > /tmp/build-$distro.log
+    DISTR=$distro /usr/local/bin/docker-compose -p $distro run --rm yade > /tmp/build-$distro.log
     dput -U yade-dem.org /tmp/pkg-$distro/*.changes
     rm -rf /tmp/pkg-$distro/*
 done
 
 docker rm -v $(docker ps -a -q -f status=exited)
-
-
