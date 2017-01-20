0cda8a4faa6af9b33115aebdc5e2ac2f1788c02d
remche <remche@remche.org>
2017-01-20 09:54:05 +0100

    cleaning trunks and cron logs

--- conf/buildall.sh
+++ conf/buildall.sh
@@ -4,10 +4,11 @@ distros=(jessie stretch unstable trusty xenial yakkety)
 
 cd docker-yade-package
 for distro in ${distros[@]}; do
+    rm -rf /tmp/pkg-$distro/*
     echo "Building for $distro"
     DISTR=$distro /usr/local/bin/docker-compose -p $distro run --rm yade > /tmp/build-$distro.log
     dput -U yade-dem.org /tmp/pkg-$distro/*.changes
-    rm -rf /tmp/pkg-$distro/*
+    rm -rf /tmp/pkg-$distro/trunk
 done
 
 docker rm -v $(docker ps -a -q -f status=exited)
